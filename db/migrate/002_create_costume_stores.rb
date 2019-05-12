782f1117a41f756105252ead5ec81066548b9ee4
Nate Tan <nktan93@gmail.com>
2019-05-12 15:21:54 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -8,8 +8,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.integer :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
-      t.timestamps :opening_time
-      t.timestamps :closing_time
+      t.integer :opening_time
+      t.integer :closing_time
     end
   end
 end
