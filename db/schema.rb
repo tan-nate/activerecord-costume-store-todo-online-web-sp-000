1e0ac8c597ae691d3e63ab439dcec5ede77417bd
Nate Tan <nktan93@gmail.com>
2019-05-12 16:06:44 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,8 @@ ActiveRecord::Schema.define(version: 3) do
     t.integer "costume_inventory"
     t.integer "num_of_employees"
     t.boolean "still_in_business"
-    t.time "opening_time"
-    t.time "closing_time"
+    t.datetime "opening_time"
+    t.datetime "closing_time"
   end
 
   create_table "costumes", force: :cascade do |t|
