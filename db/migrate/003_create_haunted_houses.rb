337c046dad0688fc13f362ace187ad1109cead9d
Nate Tan <nktan93@gmail.com>
2019-05-12 15:53:24 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,15 @@
 # Create your haunted_houses migration here
+
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.integer :theme
+      t.float :price
+      t.boolean :still_in_business
+      t.time :opening_time
+      t.time :closing_time
+    end
+  end
+end
