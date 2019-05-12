0500a61ee940cbd9bc7a14774a919eb808018f5b
Nate Tan <nktan93@gmail.com>
2019-05-12 16:05:38 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -10,6 +10,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.boolean :family_friendly
       t.date :opening_date
       t.date :closing_date
+      t.string :description
     end
   end
 end
